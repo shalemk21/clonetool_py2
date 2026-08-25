14f9c4437659b1c682ecad06ea04f7ee2d8ae1bc
Shalem Raj <shalemk21@gmail.com>
2026-08-25 11:17:28 +0530

    Add user input for name, surname, and contact number

---
+++ name.py
@@ -0,0 +1,6 @@
+name=input("Enter ur name")
+surname=input("Enter u r surname")
+contact=input("enter u r number")
+
+print("Your full name is ",name,"",surname)
+print("Your contact number is",contact)
